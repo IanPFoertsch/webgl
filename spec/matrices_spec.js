626d8ab31403bee02160770db5d2c88a00f37cd0
Ian Foertsch <IanPFo@gmail.com>
2020-11-22 12:19:09 -0500

    Added matrix handling logic

--- spec/matrices_spec.js
+++ spec/matrices_spec.js
@@ -1,6 +1,7 @@
 "use strict"
 
-import { rotate2d, translate } from "../src/matrices.js"
+import { multiply3 } from "../src/matrices.js"
+import { mat3 } from "../node_modules/gl-matrix/gl-matrix.js"
 
 
 beforeEach(function() {
@@ -20,8 +21,6 @@ beforeEach(function() {
             var diff = Math.abs(value - expectedVector[index])
 
             if (diff > 0.00001) {
-              console.log(value)
-              console.log(expectedVector[index])
               result.pass = false
               result.message = `Vector elements at index ${index} differ by ${diff}` +
               ` expected: ${expectedVector}, actual: ${actualVector}`
@@ -35,23 +34,42 @@ beforeEach(function() {
   })
 })
 
-describe("rotate2d", function() {
-  var vector = [1,1]
+// describe("rotate2d", function() {
+//   var vector = [1,1]
+//
+//   var expectedRotations = {
+//     90: [-1,1],
+//     180: [-1,-1],
+//     270: [1,-1],
+//     360: [1,1]
+//   }
+//
+//   Object.entries(expectedRotations).forEach((fixture) => {
+//     var angle = fixture[0]
+//     var expectedTransform = fixture[1]
+//     it("generates the expected Transformation", () => {
+//       expect(rotate2d(vector, angle)).toVectorEqual(expectedTransform)
+//     })
+//   })
+// })
 
-  var expectedRotations = {
-    90: [-1,1],
-    180: [-1,-1],
-    270: [1,-1],
-    360: [1,1]
-  }
 
-  Object.entries(expectedRotations).forEach((fixture) => {
-    var angle = fixture[0]
-    var expectedTransform = fixture[1]
-    it("generates the expected Transformation", () => {
-      expect(rotate2d(vector, angle)).toVectorEqual(expectedTransform)
-    })
+describe("multiply3", function() {
+  var a = [
+    1, 2, 3,
+    4, 5, 6,
+    7, 8, 9
+  ]
+
+  var b = [
+    10, 11, 12,
+    13, 14, 15,
+    16, 17, 18
+  ]
+
+
+  fit("should multiply to create the expected result", () => {
+    var result = multiply3(a, b)
+    expect(result).toVectorEqual([138, 171, 204, 174, 216, 258, 210, 261, 312])
   })
-
-
-});
+})
--- src/matrices.js
+++ src/matrices.js
@@ -1,27 +1,39 @@
 "use strict"
 
-var convertToRadians = function(angle) {
-  return angle * (Math.PI/180)
+//Note: due to low level details in opengl, we're following the programming notation of
+// rows & columns being inverted from mathematical convention
+var multiply3 = function(a, b) {
+  var c11 = (b[0] * a[0]) + (b[1] * a[3]) +  (b[2] * a[6])
+  var c12 = (b[0] * a[1]) + (b[1] * a[4]) +  (b[2] * a[7])
+  var c13 = (b[0] * a[2]) + (b[1] * a[5]) +  (b[2] * a[8])
+
+  var c21 = (b[3] * a[0]) + (b[4] * a[3]) +  (b[5] * a[6])
+  var c22 = (b[3] * a[1]) + (b[4] * a[4]) +  (b[5] * a[7])
+  var c23 = (b[3] * a[2]) + (b[4] * a[5]) +  (b[5] * a[8])
+
+  var c31 = (b[6] * a[0]) + (b[7] * a[3]) +  (b[8] * a[6])
+  var c32 = (b[6] * a[1]) + (b[7] * a[4]) +  (b[8] * a[7])
+  var c33 = (b[6] * a[2]) + (b[7] * a[5]) +  (b[8] * a[8])
+
+  return [c11, c12, c13, c21, c22, c23, c31, c32, c33]
 }
 
-var rotate2d = function(vector2d, theta) {
-  const radians = convertToRadians(theta)
-  const x = vector2d[0]
-  const y = vector2d[1]
-  const cosTheta = Math.cos(radians)
-  const sinTheta = Math.sin(radians)
+var rotationMatrix = function(radians) {
+  var sin = Math.sin(radians)
+  var cos = Math.cos(radians)
   return [
-    ((x * cosTheta ) - (y * sinTheta)),
-    ((x * sinTheta) + (y * cosTheta))
+    cos, - sin, 0,
+    sin,   cos, 0,
+    0,       0, 1
   ]
 }
 
-//pretty sure this is commutative so order doesn't matter
-var translate = function(originVector, translateVector) {
-  return originVector.map((originItem, index) => {
-    return originItem + translateVector[index]
-  })
+var translationMatrix = function(tx, ty) {
+  return [
+    1,  0,  0,
+    0,  1,  0,
+    tx, ty, 1
+  ]
 }
 
-
-export { rotate2d, translate }
+export { multiply3, rotationMatrix, translationMatrix }
